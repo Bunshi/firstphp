12ac4d0128c0efdb3a844ae928c7b1009c864013
船曳文士 <funabikibunshi@funabikibunshinoMacBook-Pro.local>
2020-10-07 09:27:49 +0900

    list 2-2

---
+++ text.php
@@ -0,0 +1,15 @@
+<?php
+print <<<HTMLBLOCK
+<html>
+<head><title>Menu</title></head>
+<body bgcolor="#fffed9">
+<h1>Dinner</h>
+<ul>
+  <li>Beef Chow-Fun</li>
+  <li>Sauteed Pea Shoots</li>
+  <li>Soy Sauce Noodles</li>  
+  </ul>
+</body>
+</html>
+HTMLBLOCK;
+?>
