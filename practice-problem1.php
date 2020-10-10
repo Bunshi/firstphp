0313de9d14e9d4ac4b933caa964ce0713d89daed
船曳文士 <funabikibunshi@funabikibunshinoMacBook-Pro.local>
2020-10-11 05:09:15 +0900

    list 2-2chapter-problem

--- practice-problem1.php
+++ practice-problem1.php
@@ -1,4 +1,5 @@
 <?php
+  // 問題１
   print 'How are you?';
   print "<br>";
   print 'I\'m fine.';
@@ -10,6 +11,7 @@
 
 <?php
   /*
+   * 問題２
    * レストランでの食事の合計金額を計算するPHPプログラムを書きなさい。
    * 400円のハンバーガーを２つ、
    * 200円のミルクシェイクを１つ、
@@ -36,6 +38,15 @@
 ?>
 
 <?php
+  /*
+   * 問題３
+   * 問題の２の解を修正して、フォーマットされた請求書を出力する。
+   * 食事の各品目について、値段、数量、合計金額を出力する。
+   * 食べ物と飲み物の税抜きの合計額、
+   * 税込の合計額、
+   * 税金とチップを含めた合計を出力する。
+   * 金額は縦に並べて出力する。
+   */ 
   $hamburger = 400;
   $shake = 200;
   $cola = 100;
@@ -60,4 +71,58 @@
   printf("%25s: %5.0f円\n", '税込価格', round($food + $tax));
   print "<br>";
   printf("%25s: %5.0f円\n", 'チップを含めた税込価格', round($food + $tip + $tax));
+  print "<br>";
+  print "<br>";
+  print "<br>";
+  /**
+   * d = 十進数
+   * s = 文字列
+   * f = 浮動小数点
+   */
 ?>
+
+<?php
+  /**
+   * 変数$first_nameに自分の名前、
+   * $last_nameに自分の姓を設定するPHPプログラムを書く。
+   * 名前と姓をスペースで区切った文字列を出力する。
+   * また、その文字列の長さも出力する。
+   */
+  $first_name = 'Bunshi';
+  $last_name = 'Funabiki';
+  $name = "$first_name $last_name";
+  print $name;
+  print strlen($name);
+  print "<br>";
+  print "<br>";
+  print "<br>";
+  // printf("%s %s", $first_name, $last_name);
+  // print strlen($first_name, $last_name);
+?>
+
+<?php
+  /**
+   * 増分演算子(++)と複合乗算演算子(*=)を使って、
+   * １から５までの数値と２(２の１乗)から３２(２の５乗)までの２の乗数を出力するPHPプログラムを書く。
+   */
+  $n = 1; $p = 2;
+  print "$n, $p";
+  print "<br>";
+
+  $n++; $p *= 2;
+  print "$n, $p";
+  print "<br>";
+
+  $n++; $p *= 2;
+  print "$n, $p";
+  print "<br>";
+
+  $n++; $p *= 2;
+  print "$n, $p";
+  print "<br>";
+
+  $n++; $p *= 2;
+  print "$n, $p\n";
+  print "<br>";
+?>
+  
